ab395dc90f4150d1bec46b72fb52d8b4e71948ad
dustycelary <fungussh@gmail.com>
2026-03-26 12:29:31 +0000

    Fixing structure

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player: 
+    def __init__(self, name):
+        self.name = name
+        self.matches_played = 0
+        self.games_won = 0 
+        self.matches_won = 0
+        self.fixtures  = [] # make this of class Fixture (add all the data in).wk
